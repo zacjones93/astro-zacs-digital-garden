30c57906d2de31fdcee75cda513f63c306891b6f
Zac Jones <zacjones93@gmail.com>
2023-11-01 16:28:01 -0600

    upgrade astro to v3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,15 @@
-
 import { defineConfig } from 'astro/config';
 import remarkToc from 'remark-toc';
-import {doubleBracketPlugin} from './plugins/backlinkPlugin.mjs'
+import { doubleBracketPlugin } from './plugins/backlinkPlugin.mjs';
 
-// https://astro.build/config
-import image from "@astrojs/image";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-import react from '@astrojs/react'
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   url: 'https://zacjones.dev',
   markdown: {
-    remarkPlugins: [remarkToc, doubleBracketPlugin],
+    remarkPlugins: [remarkToc, doubleBracketPlugin]
   },
-  integrations: [
-    react(),
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    }), 
-    tailwind(), 
-  ]
+  integrations: [react(), tailwind()]
 });
